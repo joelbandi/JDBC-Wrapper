faec081f15550abcb77e3f268165d5ace0206e16
joelbandi <joelvivekbandi@gmail.com>
2016-01-10 05:18:41 -0800

    Refactored code around and restructured everything, improved a lot of interactivity too

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@
 //ARPAN TAKE CARE OF THE USER PROOFING
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*; // Enable SQL processing
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -22,109 +24,167 @@ public class Main {
 	static Connection connection = null;
 	
 
-	public static void main(String[] arg) throws Exception
-       {
- 	   System.out.println("--->Welcome to the PIKFLIX movie database interactive terminal<---");  
+	public static void main(String[] arg) throws Exception{
 		
-		
- 	   
- 	   try{
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		}catch (Exception e){
-			System.out.println("\n"
-					+ "***********************************\n"
-					+ "com.mysql.jdbc.Driver instantiation FAILED!\n"
-					+ "***********************************\n");
-					restart();
+		/*----------------------------------environment variable initialization-------------------------------------------------*/
+		System.out.println("Initializing environment variables...");
+		try {
+		    Thread.sleep(2000);         //2 seconds
+		} catch(InterruptedException ex) {
+		    Thread.currentThread().interrupt();
 		}
+		exit = false;
+		/*----------------------------------------------------------------------------------------------------------------------*/
 
- 	   
- 	   
-		System.out.println("\n Enter username: "); username = inp.next();
-		System.out.println("Password: "); password = inp.next();
-		try{
-			connection = (Connection)DriverManager.getConnection(remote,"cl57-moviedb", "arpanjoe");
-			System.out.println("Congratulations! connection to movie database established!!");
-		}catch(Exception e){
-			System.out.println("\n"
-					+ "***********************************\n"
-					+ "FAILED to establish connection\n"
-					+ "***********************************\n");
-					restart();	
-		}
-
-
-    	   System.out.println("--->Welcome to the PIKFLIX movie database interactive terminal<---");
-    	   System.out.println("What do you want to do?");
-    	   System.out.println("1. Search movies by actor");
-    	   System.out.println("2. Add a star");
-    	   System.out.println("3. Add a customer");
-    	   System.out.println("4. Delete a customer");
-    	   System.out.println("5. Provide metadata");
-    	   System.out.println("6. Run custom SQL query");
-    	   System.out.println("Please make a choice");
-    	   choice  = inp.nextInt();
-    	   switch (choice) {
+		 
+		jdbcInit();
 		
-    	   
-    	   
-    	   
-    	case 1: 
+		System.out.println("--->Welcome to the PIKFLIX<---"); 
+		
+		loginToDatabase();
+
+		
+		/*---------------------------------------------super loop---------------------------------------------------------------*/
+		while(!exit){   
+			mainMenu();  
+		}
+		/*----------------------------------------------------------------------------------------------------------------------*/
+		System.out.println("\nbye...");
+		
+		
+	}
+
+	private static void mainMenu(){
+		System.out.println("\n\nWhat do you want to do?");
+		System.out.println("1. Search movies by actor");
+		System.out.println("2. Add a star");
+		System.out.println("3. Add a customer");
+		System.out.println("4. Delete a customer");
+		System.out.println("5. Provide metadata");
+		System.out.println("6. Run custom SQL query");
+		System.out.println("7. Logout");
+		System.out.println("\nPlease make a choice");
+		choice  = inp.nextInt();
+		switch (choice) {
+		
+		
+		case 1: 
 			System.out.println(" Please enter a name: ");
 			String a = inp.nextLine();
 			searchmovies(a);
 			break;
 			
 			
-			
-		
 		case 2:
 			System.out.println(" Please enter a name: ");
 			String b = inp.nextLine();
 			addstar(b);
-		break;
-		
-		
-		
-		
-		
-		case 3: 
+			break;
 			
+			
+		case 3: 
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			System.out.println("Adding Customer...");
 			System.out.println(" Please enter the id: ");
-			int id = inp.nextInt();			
+			int id;
+			try {
+				id = inp.nextInt();
+			} catch (InputMismatchException e1) {
+				System.out.println("You did not enter a number!!");
+				System.out.println("Try again");
+				id = inp.nextInt();
+			}			
 			String[] prompts = {
-			        "Please enter a first name: ",
-			        "Please enter a last name: ",
-			        "Please enter a credit card number: ",
-			        "Please enter an address: ",
-			        "Please enter an email id: ",
-			        "Please enter the customer's password: "
-			    };
-				ArrayList<String> ar = new ArrayList<String>();
-			    for (String prompt : prompts) {
-			        System.out.println(prompt);
-			        ar.add(br.readLine());
-			        
-			    }
+					"Please enter a first name: ",
+					"Please enter a last name: ",
+					"Please enter a credit card number: ",
+					"Please enter an address: ",
+					"Please enter an email id: ",
+					"Please enter the customer's password: "
+			};
+			ArrayList<String> ar = new ArrayList<String>();
+			for (String prompt : prompts) {
+				System.out.println(prompt);
+				try {
+					ar.add(br.readLine());
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+
+			}
 			Customer customer = new Customer(id,ar.get(0),ar.get(1),ar.get(2),ar.get(3),ar.get(4),ar.get(5));
-			addcustomer(customer.getId(),customer.getFirst_name(),customer
-					.getLast_name(),customer.getCc(),customer
-					.getAdd(),customer.getEmail(),customer.getPwd());
+			try {
+				addcustomer(customer.getId(),customer.getFirst_name(),customer.getLast_name(),customer.getCc(),customer
+						.getAdd(),customer.getEmail(),customer.getPwd());
+			} catch (SQLException e) {
+				System.out.println("Could not add customer");
+			}
+			break;
+		
+		
+		case 4:
+			break;
 			
 			
+		case 5:
+			break;
 			
+			
+		case 6:
+			break;
+			
+			
+		case 7:
+			break;
 			
 			
 		default:
-			restart();
+			System.out.println("INVALID OPTION...");
 			break;
 		}
-    		 
-    		   
-       }
 
+
+	}
+	
+	private static void halt(String e){
+		exit=true;
+		System.out.println("system halting...");
+		System.out.println("Shut down caused by "+e);
+	};
+	
+	private static void loginToDatabase(){
+		
+		System.out.println("Enter username: "); username = inp.next();
+		System.out.println("Password: "); password = inp.next();
+		try{
+			connection = (Connection)DriverManager.getConnection(remote,username,password);
+			System.out.println("Congratulations! You are connected to the Database!!");
+		}catch(SQLException e){
+			System.out.println("\n"
+					+ "***********************************\n"
+					+ "FAILED to establish connection\n"
+					+ "***********************************\n");
+			if(e.getErrorCode()==1045){
+				System.out.println("Wrong user name and//or password");
+				System.out.println("Try again");
+			}
+			halt("database connectivity issues.");	
+		}
+	}
+	
+	private static void jdbcInit(){
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		}catch (Exception e){
+			System.out.println("\n"
+					+ "*************************************************\n"
+					+ "***com.mysql.jdbc.Driver instantiation FAILED!***\n"
+					+ "*************************************************\n");
+			halt("JDBC driver failure.");
+		}
+	}
+	
+	/*----------------------------------------------------------------------------------------------------------------------*/    	   
 	private static void addcustomer(int id, String first, String last, String cc, String add, String email, String pwd) throws SQLException {
 		 Statement query = connection.createStatement();
 		 try{
@@ -136,11 +196,10 @@ public class Main {
                  + add + "','"
                  + email + "','"
                  + pwd + "');");
+		 System.out.println("Customer "+first+" "+last+" successfully added");
+		 mainMenu();
 		 }catch(SQLException e){
 			 System.out.println("Credit card does not exist");
-			 System.out.println(e);
-			 restart();
-			 
 		 }
 	}
 
@@ -154,5 +213,5 @@ public class Main {
 		
 	}
 	
-	private static void restart(){}
+
 }
